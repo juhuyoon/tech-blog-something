2d31c387b4290e2e1376166820b27f9c6c6230d4
Junghoon Yoon <jyoon3@2u.com>
2022-05-13 10:07:57 -0400

    Got content to post and then display back to the front page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,1 +1,19 @@
 const router = require('express').Router();
+const { json } = require('express/lib/response');
+const { User } = require('../../models');
+
+router.post('/sign-up', async (req, res) => {
+	try {
+		const userData = await User.create({
+			username: req.body.username,
+			password: req.body.password,
+		});
+
+		res.status(200).json(userData);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+module.exports = router;
